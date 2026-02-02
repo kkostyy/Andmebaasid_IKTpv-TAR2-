9e28cbe4582ac1c8800de08fa26006842e5e628b
kkosty <kosgaponenko@gmail.com>
2026-02-02 16:03:50 +0200

    Update 'opilane' table structure and insert data
    Added UNIQUE constraint to 'perenimi' column and modified the table structure. Inserted sample data into 'opilane' table.

--- opilane.sql
+++ opilane.sql
@@ -5,8 +5,21 @@ use gaponenkoSQL;
 CREATE TABLE opilane(
 opilaneID int PRIMARY KEY identity(1,1),
 eesnimi varchar(25),
-perenimi varchar(30) NOT null,
+perenimi varchar(30) NOT null UNIQUE,
 synniaeg date,
 aadress TEXT,
 kas_opib bit);
+--kuvab tabeli, * - kõik väljad
 SELECT * FROM opilane;
+
+--tabeli kustutamine
+DROP TABLE opilane;
+
+--andmete lisamine tabelise opilane
+INSERT INTO opilane(eesnimi, perenimi,synniaeg,kas_opib)
+VALUES ('Konstantyn','Fuksov','2009-12-02',1),
+('Kostya','Aljosha','2009-12-02',0);
+Select * from opilane;
+
+--muudame tabeli ja lisame piirangu
+ALTER TABLE opilane Alter column perenimi varchar(30) UNIQUE;
