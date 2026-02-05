c1282d93999a7edcefa75bd3ebe8402614a768b2
kkosty <kosgaponenko@gmail.com>
2026-02-05 10:38:36 +0200

    Create database and tables for book management

---
+++ raamat.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE raamatGaponenko;
+use raamatGaponenko;
+
+--tabeli zanr loomine
+CREATE TABLE zanr(
+zanrID int PRIMARY KEY identity(1,1),
+zanrNimetus varchar(50) not null,
+kirjeldus TEXT);
+Select * from zanr
+
+--zanr tabeli täitamine
+INSERT INTO zanr(zanrNimetus, kirjeldus)
+VALUES ('luuletus', 'see on luuletus  zanr');
+
+--tabeli autor loomine
+CREATE TABLE autor(
+autorID int PRIMARY KEY identity(1,1),
+eesnimi varchar(50),
+perenimi varchar(50),
+synniaasta int check(synniaasta > 1900),
+elukoht varchar(30),
+);
+Select * from autor
+
+--autor tabeli täitamine
+INSERT INTO autor(eesnimi, perenimi,synniaasta)
+VALUES ('Anton', 'Tammsaare', 1901);
+
+--tabeli uuendamine
+UPDATE autor SET elukoht = 'Eesti';
+
+--kustutamine tabelist
+DELETE FROM autor Where autorID =
+
+--raamat tabeli loomine
+CREATE TABLE raamat(
+raamatID int PRIMARY KEY identity(1,1),
+raamatNimetus varchar(100) UNIQUE,
+lk int,
+autorID int,
+FOREIGN KEY (autorID) REFERENCES autor(autorID),
+zanrID int,
+FOREIGN KEY (zanrID) REFERENCES zanr(zanrID)
+);
+
+INSERT INTO raamat (raamatNimetus, lk, autorID, zanrID)
+VALUES ('sipsik', 300, 3, 2)
+
+Select * from autor
+Select * from zanr
+Select * from raamat
+
+--trykitudRaamat tabeli loomine
+CREATE TABLE trykitudRaamat(
+trRaamatID int PRIMARY KEY identity(1,1),
+nimetus varchar(50) UNIQUE,
+
+raamatID int,
+FOREIGN KEY (raamatID) REFERENCES raamat(raamatID),
+
+);
+Select * from trykitudRaamat
+
+--trykikoda tabeli loomine
+CREATE TABLE trykikoda(
+trykikodaID int PRIMARY KEY identity(1,1),
+nimetus varchar(30) UNIQUE,
+aadress varchar(30),
+trykikodaID int,
+FOREIGN KEY (trykikodaID) REFERENCES trykitudRaamat(trykikodaID)
+);
+
+
+DROP TABLE trykitudRaamat;
